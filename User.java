3dccc3c00a12af21a3a512b780fc437ac6433cd5
周鑫 <zx1072040559@outlook.com>
2019-09-29 16:37:57 +0800

    branch_demo

---
+++ User.java
@@ -0,0 +1,16 @@
+package com.itheima.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    private Long id;
+    private String username;
+    private String password;
+    private Long email;
+}
